172aa0c8f7ea8e9a5d4418696e134e0ae79db511
Daniil Sorokin <99132300+Deadpul378@users.noreply.github.com>
2022-07-28 19:56:02 +0300

    ф

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,10 @@ function inputCheck() {
   const length = inputValueEl.dataset.length;
   let inputLength = inputValueEl.value.length.toString();
   if (length === inputLength) {
-    return inputValueEl.classList.add("valid");
+    return (
+      inputValueEl.classList.add("valid"),
+      inputValueEl.classList.remove("invalid")
+    );
   }
   inputValueEl.classList.add("invalid");
   inputValueEl.classList.remove("valid");
